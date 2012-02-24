d87a207fe2dde70e7e83e7e1d66cf5ed784cfa18
Kaushik Thirthappa <kaushik@quadnode.com>
2012-02-24 12:56:34 +0530

    renaming index view file to invites - adding beta_invite.gemspec

--- config/initializers/beta_invite.rb
+++
@@ -1,8 +0,0 @@
-# Load YAML file to configure "from" and "to" email address configurations.
-require 'ostruct'
-	app_path = File.expand_path("#{Rails.root}/config/beta_invite.yml", __FILE__)
- 	if FileTest.exist?(app_path)
-	 	BetaInviteConfig = OpenStruct.new(YAML.load_file("#{Rails.root}/config/beta_invite.yml")[Rails.env].symbolize_keys) 	
- 	else
- 	 	BetaInviteConfig = OpenStruct.new(YAML.load_file("#{$BETAINVITE_PATH}/config/beta_invite.yml")[Rails.env].symbolize_keys)	
- 	end
